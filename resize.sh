9b4fe3b3889a037d315e2ff66fdc8ffb162901c1
siddamjayachandra <siddamjayachandranaidu@outlook.com>
2024-12-19 13:04:24 +0530

    root volumes changes also taking place

--- resize.sh
+++ resize.sh
@@ -10,6 +10,12 @@ exec > $LOG_FILE 2>&1
 
 echo "Starting user data script..."
 
+# Run e2fsck to check the file system for consistency
+sudo e2fsck -f /dev/xvda1
+growpart /dev/xvda 1
+# Resize the file system to use the new volume size
+sudo resize2fs /dev/xvda1
+
 # Check if the volume is available
 if lsblk | grep -q xvdf; then
     echo "Volume /dev/xvdf found, formatting and mounting..."
